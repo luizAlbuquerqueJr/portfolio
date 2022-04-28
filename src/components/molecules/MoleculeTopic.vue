6c2537f184673dc188ba3c01975572aa4549a22d
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-04-28 11:32:49 -0300

    feat: add onInterserct

--- src/components/molecules/MoleculeTopic.vue
+++ src/components/molecules/MoleculeTopic.vue
@@ -1,11 +1,12 @@
 <template>
-  <v-row no-gutters>
+  <v-row no-gutters v-intersect.once="onIntersect">
     <h2 class="topicFontSize secondaryColor topicText bg-themePrimaryColor">{{number}}</h2>
     <h2 class="topicFontSize highlightedColor topicText bg-themePrimaryColor px-5" >{{text}}</h2>
   </v-row>
 </template>
 
 <script>
+  import utils from "../../utils/index.js"
   export default {
     props:{
       number: {
@@ -17,6 +18,13 @@
         default: ''
       }
     },
+    methods: {
+      onIntersect(entries, observer, isIntersect){
+        if(isIntersect){
+          utils.amplitudeEmit({eventType:`user viewed ${this.text}`})
+        }
+      }
+    }
   }
 </script>
 <style>
